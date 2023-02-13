fea64054e32a4dd045440b7138a72d7abb14ddbd
Marina Nasonkina <marina.nasonkina96@yandex.ru>
2023-02-13 15:57:12 +0300

    refactor: создание функции closePopup, удаление лишнего кода, перенос обновления инпутов в openPopup

--- scripts/index.js
+++ scripts/index.js
@@ -2,34 +2,28 @@ let editButton = document.querySelector('.profile__edit-button');
 let profileName = document.querySelector('.profile__name');
 let profileAbout = document.querySelector('.profile__about');
 let popup = document.querySelector('.popup');
-let editForm = popup.querySelector('.popup__container');
+let editForm = popup.querySelector('.popup__form');
 let closeButton = popup.querySelector('.popup__close-button');
 let inputName = popup.querySelector('.popup__field_type_name');
 let inputAbout = popup.querySelector('.popup__field_type_about');
 
-inputName.setAttribute('value', profileName.textContent);
-inputAbout.setAttribute('value', profileAbout.textContent);
-
-editButton.addEventListener('click', function () {
-  popup.classList.add('popup_opened');
-});
-
-function closePopup(evt) {
-  evt.preventDefault();
+function openPopup() {
   inputName.value = profileName.textContent;
   inputAbout.value = profileAbout.textContent;
-  popup.classList.remove('popup_opened');
+  popup.classList.add('popup_opened');
 }
 
-closeButton.addEventListener('click', closePopup);
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
 
 function editFormSubmit(evt) {
   evt.preventDefault();
-  inputName.setAttribute('value', inputName.value);
-  inputAbout.setAttribute('value', inputAbout.value);
   profileName.textContent = inputName.value;
   profileAbout.textContent = inputAbout.value;
-  popup.classList.remove('popup_opened');
+  closePopup();
 }
 
+editButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
 editForm.addEventListener('submit', editFormSubmit);
