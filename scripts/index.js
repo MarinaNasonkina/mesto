9de4dd8c8774970d7f05e2db1981a7c16bdcebb3
Marina Nasonkina <marina.nasonkina96@yandex.ru>
2023-02-27 19:28:28 +0300

    feat: просмотр фотографии

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,7 @@ const sectionCards = document.querySelector('.cards');
 const cardTemplate = document.querySelector('.card-template').content;
 
 const popups = document.querySelectorAll('.popup');
+const closeButtons = document.querySelectorAll('.popup__close-button');
 
 const popupEditProfile = document.querySelector('.popup_type_edit-profile');
 const editForm = document.querySelector('.popup__form_type_edit-profile');
@@ -18,7 +19,9 @@ const addPlaceForm = document.querySelector('.popup__form_type_add-place');
 const inputPlaceName = document.querySelector('.popup__field_type_place-name');
 const inputPlaceImg = document.querySelector('.popup__field_type_place-img');
 
-const closeButtons = document.querySelectorAll('.popup__close-button');
+const popupFullScreen = document.querySelector('.popup_type_full-screen-place');
+const popupImage = document.querySelector('.popup__image');
+const popupSubtitle = document.querySelector('.popup__subtitle');
 
 const initialCards = [
   {
@@ -51,11 +54,13 @@ initialCards.forEach(element => makePlace(element));
 
 function makePlace(place) {
   const cardElement = cardTemplate.cloneNode(true);
+  const cardImage = cardElement.querySelector('.card__image');
   const cardLikeButton = cardElement.querySelector('.card__like-button');
   const cardRemoveButton = cardElement.querySelector('.card__remove-button');
   cardElement.querySelector('.card__title').textContent = place.name;
-  cardElement.querySelector('.card__image').src = place.link;
-  cardElement.querySelector('.card__image').alt = place.name;
+  cardImage.src = place.link;
+  cardImage.alt = place.name;
+  cardImage.addEventListener('click', evt => openFullScreenPopup(evt));
   cardLikeButton.addEventListener('click', evt => {
     evt.target.classList.toggle('card__like-button_active');
   });
@@ -63,6 +68,13 @@ function makePlace(place) {
     evt.target.closest('.card').remove();
   });
   sectionCards.prepend(cardElement);
+}
+
+function openFullScreenPopup(evt) {
+  popupImage.src = evt.target.src;
+  popupImage.alt = evt.target.alt;
+  popupSubtitle.textContent = evt.target.alt;
+  openPopup(popupFullScreen);
 }
 
 function openPopup(popupType) {
